1bd91d755196d26b41fcf98087f39007429c5789
Teranikys <97176914+Teranikys@users.noreply.github.com>
2023-08-31 15:01:55 +0300

    Initial commit

---
+++ src/main/java/com/example/project_x/ProjectXApplication.java
@@ -0,0 +1,14 @@
+package com.example.project_x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectXApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectXApplication.class, args);
+    }
+
+
+}
